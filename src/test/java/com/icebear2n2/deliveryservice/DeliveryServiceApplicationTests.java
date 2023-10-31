c800cbb0b6d4f14cf2b5f24135f1e4444054aeb9
icebear2n2 <icebear2n2@gmail.com>
2023-10-31 11:47:24 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/icebear2n2/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icebear2n2.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
